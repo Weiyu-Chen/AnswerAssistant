c290fc67975f9e9673cb8783c5fd85e69c6e1e9e
苏广文 <>
2018-01-12 10:47:57 +0800

    添加百度搜索和悟空问答

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pytesseract
 import requests
 from PIL import (Image, ImageGrab)
+import re
 
 # 二值化算法
 def binarizing(img,threshold):
@@ -16,26 +17,6 @@ def binarizing(img,threshold):
                 pixdata[x, y] = 255
     return img
 
-
-# 去除干扰线算法
-def depoint(img):   #input: gray image
-    pixdata = img.load()
-    w,h = img.size
-    for y in range(1,h-1):
-        for x in range(1,w-1):
-            count = 0
-            if pixdata[x,y-1] > 245:
-                count = count + 1
-            if pixdata[x,y+1] > 245:
-                count = count + 1
-            if pixdata[x-1,y] > 245:
-                count = count + 1
-            if pixdata[x+1,y] > 245:
-                count = count + 1
-            if count > 2:
-                pixdata[x,y] = 255
-    return img
-
 def main():
     while input("答题框出现后按任意键开始识别(0 退出) : ") != '0':
         start = time.time()
@@ -44,17 +25,17 @@ def main():
         imgQuestion = ImageGrab.grab((1420, 180, 1880, 250))
         imgQuestion.save('./imgQuestion.png')
         iQuestion = Image.open("./imgQuestion.png")
-        ###   答案一
+        ###   选项一
         imgAnswerOne = ImageGrab.grab((1420, 330, 1880, 370))
         imgAnswerOne = imgAnswerOne.convert('L')
         imgAnswerOne = binarizing(imgAnswerOne, 220)
         imgAnswerOne.save('./imgAnswerOne.png')
-        ###   答案二
+        ###   选项二
         imgAnswerTwo = ImageGrab.grab((1420, 420, 1880, 460))
         imgAnswerTwo = imgAnswerTwo.convert('L')
         imgAnswerTwo = binarizing(imgAnswerTwo, 220)
         imgAnswerTwo.save('./imgAnswerTwo.png')
-        ###   答案三
+        ###   选项三
         imgAnswerThree = ImageGrab.grab((1420, 500, 1880, 540))
         imgAnswerThree = imgAnswerThree.convert('L')
         imgAnswerThree = binarizing(imgAnswerThree, 220)
@@ -64,15 +45,15 @@ def main():
         question = pytesseract.image_to_string(imgQuestion, lang='chi_sim')
         question = question.replace(' ','').replace('\n','')
         question = question[question.find('.') + 1:question.find('?')]
-        ###   答案一
+        ###   选项一
         answerOne = pytesseract.image_to_string(imgAnswerOne, lang='chi_sim')
         answerOne = answerOne.replace(' ','').replace('\n','')
         answerOne = answerOne[answerOne.find('.') + 1:answerOne.find('?')]
-        ###   答案二
+        ###   选项二
         answerTwo = pytesseract.image_to_string(imgAnswerTwo, lang='chi_sim')
         answerTwo = answerTwo.replace(' ','').replace('\n','')
         answerTwo = answerTwo[answerTwo.find('.') + 1:answerTwo.find('?')]
-        ###   答案三
+        ###   选项三
         answerThree = pytesseract.image_to_string(imgAnswerThree, lang='chi_sim')
         answerThree = answerThree.replace(' ','').replace('\n','')
         answerThree = answerThree[answerThree.find('.') + 1:answerThree.find('?')]
@@ -80,9 +61,36 @@ def main():
         print('选项一:' + answerOne)
         print('选项二:' + answerTwo)
         print('选项三:' + answerThree)
-        
-
-        urllib.request.urlopen('https://www.wukong.com/search/?keyword=' + question)
+        ###   搜索答案，悟空问答
+        wukongR = requests.get(url='https://www.wukong.com/wenda/web/search/brow/?search_text=' + urllib.parse.quote(question) + '&count=10')
+        if wukongResult["data"]["feed_question"][0]['ans_list']:
+            wukongResult = wukongResult["data"]["feed_question"][0]['ans_list'][0]['abstract_text']
+        else:
+            wukongResult = '无答案'
+        print('悟空问答：' + wukongResult)
+        ###   搜索答案，百度搜索
+        baiduR = requests.get('http://www.baidu.com/s?wd=' + '巴西的官方语言').text
+        baiduResult = BeautifulSoup(baiduR, 'html.parser')
+        baiduRegOne = re.compile("(?=" + answerOne + ")")
+        baiduRegTwo = re.compile("(?=" + answerTwo + ")")
+        baiduRegThree = re.compile("(?=" + answerThree + ")")
+        if answerOne:
+            baiduLengthOne = len(baiduRegOne.findall(str(baiduResult)))
+        else:
+            baiduLengthOne = '0'
+        if answerTwo:
+            baiduLengthTwo = len(baiduRegTwo.findall(str(baiduResult)))
+        else:
+            baiduLengthTwo = '0'
+        if answerThree:
+            baiduLengthThree = len(baiduRegThree.findall(str(baiduResult)))
+        else:
+            baiduLengthThree = '0'
+        print("百度搜索：")
+        print("选项一：" + baiduLengthOne)
+        print("选项二：" + baiduLengthTwo)
+        print("选项三：" + baiduLengthThree)
+        ###答题结束
         end = time.time()
         print('总共用时：' + str( end - start ) + '秒')
 
